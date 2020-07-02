ee342105a75f8c149060644d981bdf7e0f0bf705
Chris Strutton <chris@rodandfly.ca>
2020-07-02 20:05:05 +0000

    tested querying network interfaces

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+import netifaces
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/network')
+def network():
+    plant_network = netifaces.ifaddresses('eth1')
+    plant_network = plant_network[netifaces.AF_INET][0]['addr']
+
+    PLC_network = netifaces.ifaddresses('eth0')
+    PLC_network = PLC_network[netifaces.AF_INET][0]['addr']
+
+    return 'Plant:' + plant_network + ' PLC:' + PLC_network
 
 if __name__ == '__main__':
     print('starting....')
